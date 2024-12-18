ba73c40a8750099e19bb5863274ac3d995ee10a2
Ezhilvendan <134928060+ezhil020@users.noreply.github.com>
2024-12-18 15:10:22 +0530

    Time: 7 ms (58.16%) | Memory: 12.3 MB (74.04%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,7 +1,31 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        // int ans = 0;
+        int sum=0;
+        int n = s.size();
+        unordered_map<char,int> mp ;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        for(int i = 0 ; i<n-1;i++ )
+        {
+            if(mp[s[i]] >= mp[s[i+1]])
+            {
+                sum +=  mp[s[i]];
+
+            }
+            else{
+                sum-=mp[s[i]];
+            }
+        }
+        sum += mp[s[n-1]];
+        return sum;
+    }
+    // int ans = 0;
         // int val;
         // int arr[s.size()];
 
@@ -43,28 +67,4 @@ public:
         //     }
         // }
         // return ans;
-        int sum=0;
-        int n = s.size();
-        unordered_map<char,int> mp ;
-        mp['I'] = 1;
-        mp['V'] = 5;
-        mp['X'] = 10;
-        mp['L'] = 50;
-        mp['C'] = 100;
-        mp['D'] = 500;
-        mp['M'] = 1000;
-        for(int i = 0 ; i<n-1;i++ )
-        {
-            if(mp[s[i]] >= mp[s[i+1]])
-            {
-                sum +=  mp[s[i]];
-
-            }
-            else{
-                sum-=mp[s[i]];
-            }
-        }
-        sum += mp[s[n-1]];
-        return sum;
-    }
 };
