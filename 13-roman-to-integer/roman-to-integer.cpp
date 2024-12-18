07e6f185c9b9231c6b6558b4ffab4b10e7b1286b
Ezhilvendan <134928060+ezhil020@users.noreply.github.com>
2024-12-18 15:09:44 +0530

    Time: 11 ms (32.26%) | Memory: 12.3 MB (76.17%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,47 +1,70 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        int ans = 0;
-        int val;
-        int arr[s.size()];
+        // int ans = 0;
+        // int val;
+        // int arr[s.size()];
 
-        for (int i = 0; i < s.size(); i++) {
-            switch (s[i]) {
-                case 'I':
-                    val = 1;
-                    break;
-                case 'V':
-                    val = 5;
-                    break;
-                case 'X':
-                    val = 10;
-                    break;
-                case 'L':
-                    val = 50;
-                    break;
-                case 'C':
-                    val = 100;
-                    break;
-                case 'D':
-                    val = 500;
-                    break;
-                case 'M':
-                    val = 1000;
-                    break;
+        // for (int i = 0; i < s.size(); i++) {
+        //     switch (s[i]) {
+        //         case 'I':
+        //             val = 1;
+        //             break;
+        //         case 'V':
+        //             val = 5;
+        //             break;
+        //         case 'X':
+        //             val = 10;
+        //             break;
+        //         case 'L':
+        //             val = 50;
+        //             break;
+        //         case 'C':
+        //             val = 100;
+        //             break;
+        //         case 'D':
+        //             val = 500;
+        //             break;
+        //         case 'M':
+        //             val = 1000;
+        //             break;
+        //     }
+        //     arr[i] = val;
+        // }
+        // for (int i = 0; i < s.size();i++) {
+        //     if (i != s.size() - 1) {
+        //         if (arr[i] >= arr[i+1]) {
+        //             ans += arr[i];
+        //         } else {
+        //             ans -= arr[i];
+        //         }
+        //     } else {
+        //         ans += arr[i];
+        //     }
+        // }
+        // return ans;
+        int sum=0;
+        int n = s.size();
+        unordered_map<char,int> mp ;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+        for(int i = 0 ; i<n-1;i++ )
+        {
+            if(mp[s[i]] >= mp[s[i+1]])
+            {
+                sum +=  mp[s[i]];
+
             }
-            arr[i] = val;
-        }
-        for (int i = 0; i < s.size(); i++) {
-            if (i != s.size() - 1) {
-                if (arr[i] >= arr[i+1]) {
-                    ans += arr[i];
-                } else {
-                    ans -= arr[i];
-                }
-            } else {
-                ans += arr[i];
+            else{
+                sum-=mp[s[i]];
             }
         }
-        return ans;
+        sum += mp[s[n-1]];
+        return sum;
     }
 };
