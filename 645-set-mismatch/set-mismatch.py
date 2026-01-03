727d69c409691d60a6e6d6db07ca28368fddd497
Ezhilvendan <134928060+ezhil020@users.noreply.github.com>
2026-01-03 14:58:55 +0530

    Time: 4 ms (92.00%) | Memory: 13.8 MB (61.01%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findErrorNums(self,nums):
+        n = len(nums)
+        
+        expected_sum = n * (n + 1) // 2
+        actual_sum = sum(nums)
+        
+        unique_sum = sum(set(nums))
+        
+        duplicate = actual_sum - unique_sum
+        missing = expected_sum - unique_sum
+        
+        return [duplicate, missing]
+
+
+
+                
+        
